c5847d59ddf707fd4f94ec53b8bc4494669c0e88
Dhruvaraj K <dhruvarajk52@gmail.com>
2026-02-21 15:28:24 +0530

    Fixed favicon 404 by adding WhiteNoise and root URL redirect

--- elaundry/urls.py
+++ elaundry/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import health_check
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', health_check, name='health_check'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
     path('',include("myadmin.urls")),
     path('',include("user.urls")),
     path('',include("payment.urls")),
     path('',include("business.urls")),
     path('',include("logistics.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
