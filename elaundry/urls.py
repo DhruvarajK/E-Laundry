a68cbb6fc5401486713cf81f314ec464f985e37b
Dhruvaraj K <dhruvarajk52@gmail.com>
2026-02-21 14:56:34 +0530

    added  /health endpoint for status monitoring

--- elaundry/urls.py
+++ elaundry/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from .views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check, name='health_check'),
     path('',include("myadmin.urls")),
     path('',include("user.urls")),
     path('',include("payment.urls")),
